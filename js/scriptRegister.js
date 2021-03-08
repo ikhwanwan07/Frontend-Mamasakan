f4bf8e4edb4784b8c770f429214c8cbaed8c44f5
giovmahe <giovannimaendra@gmail.com>
2021-03-08 14:11:38 +0700

    Update login register form

--- js/scriptRegister.js
+++ js/scriptRegister.js
@@ -1,10 +1,27 @@
 const buttonDaftar = document.getElementsByClassName('btnDaftar')[0]
 const checkboxSyarat = document.getElementById('checkboxSyarat')
 const formControl = document.querySelectorAll('.form-control');
+const togglePassword = document.querySelector('.fas');
+var password = document.querySelector('#inputPassword');
 // console.log(checkboxSyarat);
 
 buttonDaftar.onclick = ()=>{
   buttonDaftar.style.background = "#999a9e"
+}
+
+togglePassword.onclick = () => {
+      // toggle the type attribute
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    password.setAttribute('type', type);
+    // toggle the eye slash icon
+    const icon = togglePassword.className
+    if(icon === "fas fa-eye-slash"){
+      togglePassword.classList.remove("fa-eye-slash")
+      togglePassword.classList.toggle("fa-eye")
+    } else{
+      togglePassword.classList.remove("fa-eye")
+      togglePassword.classList.toggle("fa-eye-slash")
+    }
 }
 
 checkboxSyarat.onchange = ()=>{
